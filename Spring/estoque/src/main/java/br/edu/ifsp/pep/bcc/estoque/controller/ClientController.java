d4adab77add6dbb71289ed860e62f132b23e6060
vongui <guilhermemoraes8@hotmail.com>
2025-03-06 10:29:05 -0300

    refactor: updating project to have correct layer distribution

--- Spring/estoque/src/main/java/br/edu/ifsp/pep/bcc/estoque/controller/ClientController.java
+++ Spring/estoque/src/main/java/br/edu/ifsp/pep/bcc/estoque/controller/ClientController.java
@@ -1,17 +1,16 @@
 package br.edu.ifsp.pep.bcc.estoque.controller;
 
 import br.edu.ifsp.pep.bcc.estoque.model.entities.Client;
-import br.edu.ifsp.pep.bcc.estoque.repository.ClientRepository;
+import br.edu.ifsp.pep.bcc.estoque.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("client")
@@ -29,33 +28,30 @@ public class ClientController {
     */
 
     @Autowired
-    ClientRepository clientRepository;
+    ClientService clientService;
 
-    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Client>> getAll(){
-
-        List<Client> listaClientes = clientRepository.findAll();
-        if (listaClientes.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        List<Client> listaClient = clientService.getAll();
+        if (listaClient.isEmpty()){
+            return ResponseEntity.noContent().build();
         }
-        else
-            return ResponseEntity.ok(listaClientes);
+        return ResponseEntity.ok(listaClient);
     }
 
     @GetMapping(value = "/{codigo}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Client> getClient(@PathVariable int codigo){
 
-        Optional<Client> optionalClient = clientRepository.findById(codigo);
-        if (optionalClient.isPresent()){
-            return ResponseEntity.ok(optionalClient.get());
+        Client client = clientService.getClientById(codigo);
+        if (client != null){
+            return ResponseEntity.ok(client);
         }
-
         return ResponseEntity.notFound().build(); //utilizar build se não passar um body para o erro
     }
 
     @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Client>> getByNome(@RequestParam(value = "nome", defaultValue = "") String nome){
-        List<Client> listaCliente = clientRepository.findByNomeLike("%"+nome+"%");
+        List<Client> listaCliente = clientService.getClientByNome(nome);
         if (listaCliente.isEmpty()){
             return ResponseEntity.notFound().build();
         }
@@ -67,46 +63,36 @@ public class ClientController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Client> createClient(@RequestBody Client entity){
-        clientRepository.save(entity);
+        clientService.create(entity);
         URI uriAddress = UriComponentsBuilder.fromPath("/client/{codigo}").
-                buildAndExpand(entity.getCodigo()).toUri(); // cria o caminho
-        
+                buildAndExpand(entity.getCodigo()).toUri();
         return ResponseEntity.created(uriAddress).body(entity);
     }
 
     @DeleteMapping(value = "/{codigo}",
                 produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Client> deleteClient(@PathVariable int codigo){
-        Optional<Client> cliente = clientRepository.findById(codigo);
-        if (cliente.isPresent()){
-            clientRepository.delete(cliente.get());
-            return ResponseEntity.ok(cliente.get());
-        }
-        return ResponseEntity.notFound().build();
+        Client client = clientService.delete(codigo);
+       if (client != null){
+           return ResponseEntity.ok(client);
+       }
+       return ResponseEntity.notFound().build();
     }
 
     @PutMapping(value = "/{codigo}",
                 consumes = MediaType.APPLICATION_JSON_VALUE,
                 produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Client> updateClient(@PathVariable int codigo, @RequestBody Client client){
-        Optional<Client> optionalClient = clientRepository.findById(client.getCodigo());
 
-        if (optionalClient.isPresent()){
-            client.setCodigo(codigo);
-            Client clientAlter = clientRepository.save(client);
+    public ResponseEntity<Client> updateClient(@PathVariable int codigo, @RequestBody Client client){
+        Client clientAlter = clientService.updateClient(codigo, client);
+        if (clientAlter != null){
             return ResponseEntity.ok(clientAlter);
         }
-
         return ResponseEntity.notFound().build();
     }
-
     @PatchMapping(value = "/{codigo}/ativar")
     public ResponseEntity<String> activateClient(@PathVariable int codigo){
-        Optional<Client> optionalCliente = clientRepository.findById(codigo);
-        if (optionalCliente.isPresent()){ //cliente encontrado
-            Client c = optionalCliente.get(); // extraindo a entity do Optional
-            c.setAtivo(1);
-            clientRepository.save(c);
+        if(clientService.alterStatus(codigo, 1)){
             return ResponseEntity.ok(null);
         }
         return ResponseEntity.notFound().build();
@@ -114,15 +100,10 @@ public class ClientController {
 
     @PatchMapping(value = "/{codigo}/desativar")
     public ResponseEntity<String> deactivateClient(@PathVariable int codigo){
-        Optional<Client> optionalCliente = clientRepository.findById(codigo);
-        if (optionalCliente.isPresent()){ //cliente encontrado
-            Client c = optionalCliente.get(); // extraindo a entity do Optional
-            c.setAtivo(0);
-            clientRepository.save(c);
+        if (clientService.alterStatus(codigo, 0)){
             return ResponseEntity.ok(null);
         }
         return ResponseEntity.notFound().build();
     }
-
 
 }
