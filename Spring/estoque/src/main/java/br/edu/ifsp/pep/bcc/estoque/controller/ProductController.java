a792f46def2e9c57e30cce04de53716e7be3c680
Guilherme Moraes <67127485+Vongui@users.noreply.github.com>
2025-03-12 13:03:48 -0300

    add: add injection annotation

--- Spring/estoque/src/main/java/br/edu/ifsp/pep/bcc/estoque/controller/ProductController.java
+++ Spring/estoque/src/main/java/br/edu/ifsp/pep/bcc/estoque/controller/ProductController.java
@@ -3,6 +3,7 @@ package br.edu.ifsp.pep.bcc.estoque.controller;
 
 import br.edu.ifsp.pep.bcc.estoque.model.entities.Product;
 import br.edu.ifsp.pep.bcc.estoque.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("product")
 public class ProductController {
 
-    @Autowired
-    ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
     @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Product> getByDescricao(@RequestParam(value = "descricao", defaultValue = "") String descricao){
