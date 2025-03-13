734f311a98f41818b2c2402322e166abbd60dc33
vongui <guilhermemoraes8@hotmail.com>
2025-03-13 10:49:01 -0300

    refactor: add tratement errors

--- Spring/estoque/src/main/java/br/edu/ifsp/pep/bcc/estoque/controller/advice/ControlAdvice.java
+++ Spring/estoque/src/main/java/br/edu/ifsp/pep/bcc/estoque/controller/advice/ControlAdvice.java
@@ -1,20 +1,44 @@
 package br.edu.ifsp.pep.bcc.estoque.controller.advice;
 
+import br.edu.ifsp.pep.bcc.estoque.controller.exception.ApiError;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-@ControllerAdvice
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
 public class ControlAdvice {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public String handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
 
-        return "Campo: " + ex.getBindingResult().getFieldError().getField() +
-                " - Descrição erro: " + ex.getBindingResult().getFieldError().getDefaultMessage();
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> genericException(Exception ex) {
+        ApiError apiError = ApiError
+                .builder()
+                .timestamp(LocalDateTime.now())
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                .errors(List.of(ex.getMessage()))
+                .build();
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> argumentNotValidException(MethodArgumentNotValidException ex) {
+        List<String> errorList = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+        ApiError apiError = ApiError
+                .builder()
+                .timestamp(LocalDateTime.now())
+                .code(HttpStatus.BAD_REQUEST.value())
+                .status(HttpStatus.BAD_REQUEST.name())
+                .errors(errorList)
+                .build();
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
 }
