ae953e3c5c2a5751001228157f7facc9d9495464
vongui <guilhermemoraes8@hotmail.com>
2025-03-13 10:23:55 -0300

    fix: alter param "condigo" to "codigo"

--- Spring/estoque/src/main/java/br/edu/ifsp/pep/bcc/estoque/controller/dto/ClientResponseDTO.java
+++ Spring/estoque/src/main/java/br/edu/ifsp/pep/bcc/estoque/controller/dto/ClientResponseDTO.java
@@ -2,7 +2,7 @@ package br.edu.ifsp.pep.bcc.estoque.controller.dto;
 
 import br.edu.ifsp.pep.bcc.estoque.model.entities.Client;
 
-public record ClientResponseDTO(int condigo ,String nome, String email, String telefone) {
+public record ClientResponseDTO(int codigo ,String nome, String email, String telefone) {
     public static ClientResponseDTO transformaEmDto(Client client){
         return new ClientResponseDTO(client.getCodigo() ,client.getNome(), client.getEmail(), client.getTelefone());
     }
