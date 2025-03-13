e41e8af405252650eb9482f58b98d68059ea8128
vongui <guilhermemoraes8@hotmail.com>
2025-03-13 09:59:37 -0300

    rafactor: alter class name GlobalExceptionHandler to ControlAdvice

--- Spring/estoque/src/main/java/br/edu/ifsp/pep/bcc/estoque/controller/advice/ControlAdvice.java
+++ Spring/estoque/src/main/java/br/edu/ifsp/pep/bcc/estoque/controller/advice/ControlAdvice.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class ControlAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
