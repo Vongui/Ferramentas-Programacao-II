f7dc7dae74613d8bd536aace79b9ce3b19b9aba2
Guilherme Moraes <67127485+Vongui@users.noreply.github.com>
2025-03-20 06:46:49 -0300

    feat: adicionando aplicação

---
+++ Spring/controleServicos/src/main/java/br/edu/ifsp/pep/bcc/controle/demo/ControleOrdensDeServicoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.pep.bcc.controle.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleOrdensDeServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleOrdensDeServicoApplication.class, args);
+	}
+
+}
---
+++ Spring/controleServicos/src/test/java/br/edu/ifsp/pep/bcc/controle/demo/ControleOrdensDeServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.pep.bcc.controle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleOrdensDeServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
