1941801501856393a7d48c57b35b2976e2d60731
vongui <guilhermemoraes8@hotmail.com>
2025-04-10 10:31:52 -0300

    add: add exception nocontent

---
+++ Spring/controleServicos/src/main/java/br/edu/ifsp/pep/bcc/controle/demo/controller/exception/NoContent.java
@@ -0,0 +1,7 @@
+package br.edu.ifsp.pep.bcc.controle.demo.controller.exception;
+
+public class NoContent extends RuntimeException {
+    public NoContent(String message) {
+        super(message);
+    }
+}
