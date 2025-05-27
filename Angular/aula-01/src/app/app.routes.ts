b3b7b0d1ba9aaab3169bc1aaf5989d65314e4d20
Vongui <guilhermemoraes8@hotmail.com>
2025-05-27 10:54:36 -0300

    feat: adicionando botao, imagens e estilizando

--- Angular/aula-01/src/app/app.routes.ts
+++ Angular/aula-01/src/app/app.routes.ts
@@ -1,3 +1,23 @@
+
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { PostComponent } from './components/post/post.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/cliente/cliente.component').then(
+        (m) => m.ClienteComponent
+      )
+  },
+
+  {
+    path: 'post',
+    loadComponent: () =>
+      import('./components/post/post.component').then(
+        (m) => m.PostComponent
+      )
+  }
+];
+
